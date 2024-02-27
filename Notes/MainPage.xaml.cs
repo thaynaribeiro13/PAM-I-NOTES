bbf990aaa6dd293c0dbad52ee9d0605006cc65d2
Thayná <ribeirothayna640@gmail.com>
2024-02-26 21:44:17 -0300

    feat: add initial project

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        //Declaração de variável: tipo nomedaVariavel = 0  
+        int count = 0;
+        string nome = ""; 
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnNameClicked(object sender, EventArgs e)
+        {
+            nome = NameEntry.Text;
+            // Interpolação
+            GreetingLabel.Text = $"Olá, {nome}";
+
+            SemanticScreenReader.Announce(GreetingLabel.Text);
+        }
+    }
+
+}
